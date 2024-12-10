bf318bf5b455421b6f4995273810a28c0cb76245
Chirag Mahajan <95704426+Cryptdroid@users.noreply.github.com>
2024-12-10 12:54:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-import streamlit as st
-import pandas as pd
-
 def authenticate():
     st.title("Secure Login")
     password = st.text_input("Enter the password:", type="password")
